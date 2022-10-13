0a415fb61d4d7f3719b3cff73abb5e6779e8e33b
Konrad Stawiski MD PhD <konrad@konsta.com.pl>
2022-10-13 03:09:07 +0000

    1

--- Validation/validate.sh
+++ Validation/validate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+wget https://github.com/broadinstitute/cromwell/releases/download/84/womtool-84.jar -o /workspaces/seq-pipeline/Validation/womtool.jar
+
 
 # KONSTA_WES_TumorOnly.wdl
-java -jar /workspaces/seq-pipeline/Validation/womtool-84.jar validate /workspaces/seq-pipeline/WES_TumorOnly/KONSTA_WES_TumorOnly.wdl
-java -jar /workspaces/seq-pipeline/Validation/womtool-84.jar inputs /workspaces/seq-pipeline/WES_TumorOnly/KONSTA_WES_TumorOnly.wdl > /workspaces/seq-pipeline/WES_TumorOnly/KONSTA_WES_TumorOnly.json
+java -jar /workspaces/seq-pipeline/Validation/womtool.jar validate /workspaces/seq-pipeline/WES_TumorOnly/KONSTA_WES_TumorOnly.wdl
+java -jar /workspaces/seq-pipeline/Validation/womtool.jar inputs /workspaces/seq-pipeline/WES_TumorOnly/KONSTA_WES_TumorOnly.wdl > /workspaces/seq-pipeline/WES_TumorOnly/KONSTA_WES_TumorOnly.json
