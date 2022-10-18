2c8f5647d9b1c142fa3152796054c0d5fa656e9d
Konrad Stawiski <konrad@konsta.com.pl>
2022-10-18 21:32:56 +0000

    a

--- Docker_init.sh
+++ Docker_init.sh
@@ -5,9 +5,9 @@ cd /
 git clone https://github.com/kstawiski/seq-pipeline
 
 # set up code-server
-export PASSWORD=konrad
-nohup code-server --password --disable-update-check &
-nohup /ngrok http 8080 --log=stdout > /ngrok.log &
+# export PASSWORD=konrad
+# nohup code-server --password --disable-update-check &
+# nohup /ngrok http 8080 --log=stdout > /ngrok.log &
 
 conda init bash
 /bin/bash
